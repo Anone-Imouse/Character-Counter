2e82740e9d00b67a0fa852baf4bf46e9746bd361
Anone-Imouse <73369548+Anone-Imouse@users.noreply.github.com>
2020-10-29 15:36:54 -0400

    Update and rename main.js to .js

--- main.js
+++
@@ -1,5 +0,0 @@
-function letter_number() {
-  var pa=document.getElementById("pa");
-  var inp=document.getElementById("inp").value;
-  pa.innerHTML="There were "+inp.length+" characters in your last search."
-}
