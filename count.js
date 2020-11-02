a468a9a18da1b41dbcb2d3ad3d7fcdf9b2a26568
Anone-Imouse <73369548+Anone-Imouse@users.noreply.github.com>
2020-11-02 18:24:11 -0500

    Update count.js

--- count.js
+++ count.js
@@ -8,3 +8,5 @@ function letter_number()
 pa.innerHTML="There were "+inp.length+ " characters in your last search.";
   //It's saying that the innerHTML so basically what you normally determine will be the text "There were " followed by inp.length(the number of characters in the input bar) proceeded by " carachters in your last search." Giving by example, if you wrote hey, it would write: "There were 3 characters in your last search."
 }
+
+console.log("If you can see this you are an absolute legend and you literally deserve an award");
